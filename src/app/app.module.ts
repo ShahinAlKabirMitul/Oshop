f12d22b505f914bf818b81c675afd0029b196c42
mitulbd <mitul.cse@gmail.com>
2017-09-30 14:17:37 +0600

    Add Shopping cart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,15 @@ import { AppComponent } from './app.component';
 import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
 import { HomeComponent } from './home/home.component';
 import { ProductsComponent } from './products/products.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BsNavbarComponent,
     HomeComponent,
-    ProductsComponent
+    ProductsComponent,
+    ShoppingCartComponent
   ],
   imports: [
     BrowserModule,
