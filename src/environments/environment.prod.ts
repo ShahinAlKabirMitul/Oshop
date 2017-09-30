1acfdf86464a1a203679631dcd526fc3b613e81e
mitulbd <mitul.cse@gmail.com>
2017-09-30 13:24:45 +0600

    Confiqure with firebase  && install firebase with " npm i --save firebase@4.2.0 angularfire2@4.0.0-rc.1" this  commnd

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyDTS8RojzIDjqCrDT9FLRL1BETvQKX46-M",
+    authDomain: "oshop-529fa.firebaseapp.com",
+    databaseURL: "https://oshop-529fa.firebaseio.com",
+    projectId: "oshop-529fa",
+    storageBucket: "",
+    messagingSenderId: "651771853859"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyDTS8RojzIDjqCrDT9FLRL1BETvQKX46-M",
+    authDomain: "oshop-529fa.firebaseapp.com",
+    databaseURL: "https://oshop-529fa.firebaseio.com",
+    projectId: "oshop-529fa",
+    storageBucket: "",
+    messagingSenderId: "651771853859"
+  }
 };
