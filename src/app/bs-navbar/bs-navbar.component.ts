8dfce2a04f64fd8c63b1a44411e243a64da79ee5
mitulbd <mitul.cse@gmail.com>
2017-09-30 19:39:52 +0600

    Display User

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -2,16 +2,20 @@
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Component, OnInit } from '@angular/core';
 
+import * as firebase from 'firebase';
+
 @Component({
   selector: 'bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
 export class BsNavbarComponent  {
+user:firebase.User;
 
   constructor(private afAuth:AngularFireAuth) {
     afAuth.authState.subscribe(s=>{
       console.log(s);
+      this.user=s;
     });
    }
 
