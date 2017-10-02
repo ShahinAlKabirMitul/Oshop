2dc37c2ae23b285066aaa800115e855f05fa884b
mitulbd <mitul.cse@gmail.com>
2017-10-03 00:26:30 +0600

    adding boostrap card

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 
 import { ProductService } from '../../product.service';
 import { CategoryService } from './../../category.service';
@@ -11,13 +12,17 @@ import { Component, OnInit } from '@angular/core';
 export class ProductFormComponent implements OnInit {
 
   categories$;
-  constructor(categoryService:CategoryService,private productService:ProductService) {
+  constructor(
+    private router:Router,
+    private categoryService:CategoryService,
+    private productService:ProductService) {
     this.categories$=categoryService.getCategories();
    }
 
    save(product){
     console.log(product);
     this.productService.create(product);
+    this.router.navigateByUrl('/admin/products')
    }
 
   ngOnInit() {
