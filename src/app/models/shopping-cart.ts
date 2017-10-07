a0cb33b0d0f86fa5ad2832c543466a423d44f349
mitulbd <mitul.cse@gmail.com>
2017-10-07 13:49:23 +0600

    better Object Initialization

--- src/app/models/shopping-cart.ts
+++ src/app/models/shopping-cart.ts
@@ -10,10 +10,7 @@ export class ShoppingCart{
         
         for(let productId in itemsMap){
             let item=itemsMap[productId];
-            let x=new ShoppingCartItem();
-            x.$key=productId;
-            Object.assign(x,item);
-            this.items.push(x);
+            this.items.push(new ShoppingCartItem({ ...item, $key:productId }));
         }
             
     }
